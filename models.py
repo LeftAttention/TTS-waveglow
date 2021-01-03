1175f3f1d711a736b5e7a7af6a4a393a9546ad13
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-03 13:16:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from tacotron2.model import Tacotron2
 from waveglow.model import WaveGlow
 import torch
+
+def model_parser(model_name, parser, add_help=False):
+    if model_name == 'Tacotron2':
+        from tacotron2.arg_parser import tacotron2_parser
+        return tacotron2_parser(parser, add_help)
+    if model_name == 'WaveGlow':
+        from waveglow.arg_parser import waveglow_parser
+        return waveglow_parser(parser, add_help)
+    else:
+        raise NotImplementedError(model_name)
